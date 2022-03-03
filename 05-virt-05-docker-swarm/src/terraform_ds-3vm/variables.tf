f3ad6caa082d216a9e4189cf7009670a72d931d5
Dmkazanskii <dmkazanskii@yandex.ru>
2022-03-03 21:05:03 +0300

    20220303_2105: homeWork update with 15

---
+++ 05-virt-05-docker-swarm/src/terraform_ds-3vm/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g33tisd4sbvsb0acjg"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gk80c3r1s2rkhjud1g"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8fled25kvd6f2iudq2"
+}
